96cf1a3b4fe802de83761c3fc336773f687a13a9
Thomas Thiery <thomas-thiery@th-th.com>
2021-06-15 00:54:36 -0400

    fix tests

--- src/app/factories/factories.component.spec.ts
+++ src/app/factories/factories.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { FactoriesComponent } from './factories.component';
 
 describe('FactoriesComponent', () => {
@@ -8,6 +8,7 @@ describe('FactoriesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ FactoriesComponent ]
     })
     .compileComponents();
